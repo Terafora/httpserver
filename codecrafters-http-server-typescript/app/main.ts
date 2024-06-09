5a4097ab6a200f7da2cd82bfc8ca24db7b434df6
Charlotte Stone <charlie.stone649@gmail.com>
2024-06-09 09:43:32 +0100

    Next step for basic res, req

--- codecrafters-http-server-typescript/app/main.ts
+++ codecrafters-http-server-typescript/app/main.ts
@@ -1,13 +1,17 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-    socket.end();
+    socket.on('data', (data) => {
+        const req : string = data.toString();
+        const path : string = req.split(' ')[1];
+        const res : string = path === '/' ? 'HTTP/1/1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
+        socket.write(res);
+        socket.end();
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
     console.log('Server is running on port 4221');
 });
